9c8bf293b6adec5213c7dec853bb297e0050a543
Phil Wamba <philwamba@gmail.com>
2019-03-06 23:05:23 +0300

    first commit

--- index.js
+++ index.js
@@ -1,30 +1,26 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
-const logger = require('morgan')
+const morgan = require('morgan')
 
 const port = process.env.PORT || 3030
 
-app.use(logger('dev'))
+app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('*', (req, res) => {
-  res.send('This is tutorial App on creating your first USSD app in 5 minutes or less by Ajala Abdulsamii <kgasta@gmail.com>')
+  res.send('A simple node js USSD app.')
 })
 
 app.post('*', (req, res) => {
   let {sessionId, serviceCode, phoneNumber, text} = req.body
   if (text == '') {
     // This is the first request. Note how we start the response with CON
-    let response = `CON What would you want to check
-    1. My Account
-    2. My phone number`
+    let response = 'CON What would you want to check \n1. My Account \n2. My phone number'
     res.send(response)
   } else if (text == '1') {
     // Business logic for first level response
-    let response = `CON Choose account information you want to view
-    1. Account number
-    2. Account balance`
+    let response = 'CON Choose account information you want to view \n1. Account number\n2. Account balance'
     res.send(response)
   } else if (text == '2') {
     // Business logic for first level response
@@ -32,18 +28,18 @@ app.post('*', (req, res) => {
     res.send(response)
   } else if (text == '1*1') {
     // Business logic for first level response
-    let accountNumber = 'ACC1001'
+    let accountNumber = 'A/C283041001'
     // This is a terminal request. Note how we start the response with END
     let response = `END Your account number is ${accountNumber}`
     res.send(response)
   } else if (text == '1*2') {
     // This is a second level response where the user selected 1 in the first instance
-    let balance = 'NGN 10,000'
+    let balance = 'KES 5,000'
     // This is a terminal request. Note how we start the response with END
     let response = `END Your balance is ${balance}`
     res.send(response)
   } else {
-    res.status(400).send('Bad request!')
+    res.status(400).send('Invalid request!')
   }
 })
 
